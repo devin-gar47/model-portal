b181a7441cee6a7f0197e94a7539b664a73cca62
devin-gar47 <devingar18@gmail.com>
2022-01-21 12:54:12 -0600

    Initial state of model portal with next

---
+++ components/MyIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { IconType } from "react-icons"
+
+interface Props {
+    Icon: IconType;
+    sport: string;
+    iconBackgroundColor: string
+}
+
+const MyIcon: React.FC<Props> = ({Icon,sport,iconBackgroundColor}) => {
+    return (
+        <>
+        <div className={`flex flex-col items-center justify-center p-3 h-24 w-24 border-b-2
+         hover:scale-110 hover:cursor-pointer transition delay-100 ease-out duration-500`}>
+                <Icon fontSize={30} color={iconBackgroundColor} />
+            <p className='text-slate-600'>{sport}</p>
+        </div>
+        </>
+    )
+}
+
+export default MyIcon
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        myBlue: '#1B2040',
+      },
+    },
+  },
+  plugins: [],
+}
