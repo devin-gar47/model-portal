de0400a7d857c6be9f7c765639de7db2b4631153
devin-gar47 <devingar18@gmail.com>
2022-01-22 20:36:25 -0600

    installed redux and devtools

---
+++ redux/actions/actionCreators.ts
@@ -0,0 +1,9 @@
+import { Action } from '@reduxjs/toolkit';
+import { DataType } from '../../utils/types/types';
+import * as actionTypes from './actionTypes'
+
+function updateTable(data: DataType){
+    return 0;
+}
+
+export {updateTable}
