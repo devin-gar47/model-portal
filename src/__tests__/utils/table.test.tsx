6e05f0844f7496b8014de3f84cbefc20614ec7fe
devin-gar47 <devingarrett@Devins-MacBook-Pro-2.local>
2022-02-27 01:13:46 -0600

    validating text input

---
+++ src/__tests__/utils/table.test.tsx
@@ -0,0 +1,20 @@
+import { isValidRecordInfo } from '../../utils/table'
+
+describe('Table utility functions', () => {
+    it('should return true because string is valid', () => {
+        expect(isValidRecordInfo('0-0')).toBe(true)
+        expect(isValidRecordInfo('   0-0     ')).toBe(true)
+        expect(isValidRecordInfo('   0   -  0     ')).toBe(true)
+    })
+
+    it('should return false because string is invalid', () => {
+        expect(isValidRecordInfo('00--')).toBe(false)
+        expect(isValidRecordInfo('00-')).toBe(false)
+        expect(isValidRecordInfo('00')).toBe(false)
+        expect(isValidRecordInfo('0-0gb')).toBe(false)
+        expect(isValidRecordInfo('00 0%')).toBe(false)
+        expect(isValidRecordInfo('something random')).toBe(false)
+        expect(isValidRecordInfo('')).toBe(false)
+        expect(isValidRecordInfo('     ')).toBe(false)
+    })
+})
