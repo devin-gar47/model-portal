acda53a468ba83c5c99a259ee0e69fb8b10476fb
devin-gar47 <devingar18@gmail.com>
2022-03-21 17:42:20 -0500

    created login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { Provider } from 'react-redux'
+import Login from '../components/page-entry/Login'
+import { store } from '../redux/store'
+
+const LoginPage: NextPage<{}, {}> = () => {
+    return (
+        <Provider store={store}>
+            <Login />
+        </Provider>
+    )
+}
+
+export default LoginPage
