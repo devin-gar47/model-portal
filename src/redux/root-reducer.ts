ba16d9baa7b2a15b51aea94bde7998fee46ea881
devin-gar47 <devingar18@gmail.com>
2022-03-23 17:59:22 -0500

    calling API for calculation table

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -4,9 +4,11 @@ import baseballHomeDivision2017 from './reducers/baseball/division/baseballHomeD
 import baseballHomeNonDivision2022 from './reducers/baseball/non-division/baseballHomeNonDivision2022'
 import baseballHomeDivision2022 from './reducers/baseball/division/baseballHomeDivision2022'
 import userInformation from './reducers/user/user-information'
+import homeCalculationTable from './reducers/baseball/calculation-table/home/home-calculation-table'
 
 const rootReducer = combineReducers({
     userInformation,
+    homeCalculationTable,
     baseballHomeNonDivision2017,
     baseballHomeDivision2017,
     baseballHomeNonDivision2022,
