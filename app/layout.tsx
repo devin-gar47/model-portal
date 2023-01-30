b3a623689bbc9bbf26440ce5c888889742f9ac8d
devin-gar47 <devingar18@gmail.com>
2023-01-30 00:59:57 -0600

    Ignoring files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
 import '../globals.css'
+import React from 'react'
 
 export default function RootLayout({
   children,
